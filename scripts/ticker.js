9d1b19b629a2de734d2dd5fda719c495727c2e47
Karel Minarik <karmi@karmi.cz>
2012-09-12 18:16:44 +0200

    Added and expanded view to the news ticker which displays all items

--- scripts/ticker.js
+++ scripts/ticker.js
@@ -8,6 +8,16 @@ jQuery.fn.ticker = function(options) {
     jQuery.fn.ticker.container.append( jQuery('<p class="loading">Loading...</p>') )
     jQuery.getJSON(options.url+'?jsoncallback=?')
 
+    jQuery.fn.ticker.container.find('h3 a')
+      .mouseover(  function() { if (!jQuery.fn.ticker.container.hasClass('expanded')) jQuery.fn.ticker.all() } )
+
+    var hide_timer = null;
+    jQuery.fn.ticker.container
+      .mouseenter( function() { clearTimeout(hide_timer) } )
+      .mouseleave( function() { if (jQuery.fn.ticker.container.hasClass('expanded')) {
+        hide_timer = setTimeout( function() { jQuery.fn.ticker.cycle() }, 250 )
+      } } )
+
     jQuery.fn.ticker.container.__initialize = function() {
       var c = jQuery.fn.ticker.container
         , current = 0
@@ -43,6 +53,18 @@ jQuery.fn.ticker.load = function(data) {
   this.container.append( jQuery("<ul></ul>").html( html ) )
   this.container.__initialize()
 };
+
+jQuery.fn.ticker.all = function(data) {
+  clearInterval(jQuery.fn.ticker.interval)
+  this.container.find('li').stop(true, true)
+  this.container.addClass("expanded")
+};
+
+jQuery.fn.ticker.cycle = function(data) {
+  this.container.removeClass("expanded")
+  this.container.__initialize()
+};
+
 
 jQuery.fn.ticker.defaults = {
   interval: 3000,
